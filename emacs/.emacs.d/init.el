241069640c7dbf264858f7b16121cc743e09bc33
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-02-09 10:22:38 +0100

    Update safe variables

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,8 +6,14 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    (quote
-    ((coq-prog-args "-emacs-U" "-R" "/home/moritz/docs/books/typetheory/cpdt/src" "Cpdt")
-     (projectile-project-compilation-cmd . "cd Horn/LLVM/reve/build && make -j8")))))
+    ((eval if
+           (string-equal
+            (file-name-extension
+             (buffer-file-name))
+            "h")
+           (c++-mode))
+     (projectile-project-compilation-cmd . "cd reve/reve/build && make -j8")
+     (coq-prog-args "-emacs-U" "-R" "/home/moritz/docs/books/typetheory/cpdt/src" "Cpdt")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
