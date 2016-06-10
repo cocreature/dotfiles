4079cdb9f5a8d7b5f1f33ef0fbf1502fe3c2465c
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-06-10 14:45:12 +0200

    Cleanup emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,6 @@
 ;(package-initialize)
 ;(setq server-name "server-proxied")
 (setq load-prefer-newer t)
-(load "~/.emacs.d/emacs.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -9,17 +8,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("613a7c50dbea57860eae686d580f83867582ffdadd63f0f3ebe6a85455ab7706" default)))
+    ("6bf237d23440fb0b340f4336695f2a08c6b785aa98288b3313526e76c38bca19" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "613a7c50dbea57860eae686d580f83867582ffdadd63f0f3ebe6a85455ab7706" default)))
  '(debug-on-error nil)
  '(org-agenda-files
    (quote
     ("~/org/satsmtar.org" "~/docs/studium/proseminar/notes.org" "/home/moritz/org/bachelor.org" "/home/moritz/org/einkauf.org" "/home/moritz/org/ghcerrors.org" "/home/moritz/org/hie.org" "/home/moritz/org/job.org" "/home/moritz/org/notes.org" "/home/moritz/org/org.org" "/home/moritz/org/recipe.org" "/home/moritz/org/travel.org" "/home/moritz/org/typetheory.org")))
  '(package-selected-packages
    (quote
-    (ws-butler bison-mode org-ref outline-magic outshine julia-shell julia-mode irfc keyfreq yaml-mode which-key web-mode volume use-package undo-tree unbound tuareg travis toml-mode sx sml-mode smart-mode-line rust-mode rainbow-delimiters prop-menu projectile pos-tip pkgbuild-mode pdf-tools paredit paradox nyan-mode nginx-mode multiple-cursors moe-theme mmm-mode magit-svn lua-mode llvm-mode libmpdee ledger-mode json-mode jonprl-mode irony-eldoc gnuplot github-issues flycheck-package flycheck-irony flx expand-region eww-lnum ensime emmet-mode elnode elisp-slime-nav dockerfile-mode docker dash-functional counsel company-irony company-coq company-cmake cmake-ide clang-format cdlatex cask auctex aggressive-indent ace-window)))
+    (graphviz-dot-mode ws-butler bison-mode org-ref outline-magic outshine julia-shell julia-mode irfc keyfreq yaml-mode which-key web-mode volume use-package undo-tree unbound tuareg travis toml-mode sx sml-mode smart-mode-line rust-mode rainbow-delimiters prop-menu projectile pos-tip pkgbuild-mode pdf-tools paredit paradox nyan-mode nginx-mode multiple-cursors moe-theme mmm-mode magit-svn lua-mode llvm-mode libmpdee ledger-mode json-mode jonprl-mode irony-eldoc gnuplot github-issues flycheck-package flycheck-irony flx expand-region eww-lnum ensime emmet-mode elnode elisp-slime-nav dockerfile-mode docker dash-functional counsel company-irony company-coq company-cmake cmake-ide clang-format cdlatex cask auctex aggressive-indent ace-window)))
  '(safe-local-variable-values
    (quote
-    ((buffer-file-coding-system . utf-8-unix)
+    ((projectile-project-compilation-cmd . "cd Horn/LLVM/reve/build && make -j8")
+     (buffer-file-coding-system . utf-8-unix)
      (eval remove-hook
            (quote write-contents-functions)
            (quote delete-trailing-whitespace))
@@ -66,8 +66,7 @@
             "h")
            (c++-mode))
      (projectile-project-compilation-cmd . "cd reve/reve/build && make -j8")
-     (coq-prog-args "-emacs-U" "-R" "/home/moritz/docs/books/typetheory/cpdt/src" "Cpdt"))))
- '(yas-global-mode t nil (yasnippet)))
+     (coq-prog-args "-emacs-U" "-R" "/home/moritz/docs/books/typetheory/cpdt/src" "Cpdt")))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -78,3 +77,4 @@
 
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+(load "~/.emacs.d/emacs.el")
