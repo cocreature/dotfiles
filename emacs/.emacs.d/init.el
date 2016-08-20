84c787d3c4e3d144d0f0783a9194bd53da529612
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2016-08-20 09:49:47 +0200

    Parens for the parent god

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,17 +8,21 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6bf237d23440fb0b340f4336695f2a08c6b785aa98288b3313526e76c38bca19" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "613a7c50dbea57860eae686d580f83867582ffdadd63f0f3ebe6a85455ab7706" default)))
+    ("cfa7053f155661faa33ef648f55d524eb97854f8f0ff9ff91a08b3ba47a9a25f" "70b5e506efe625e6a9cf71ddd5dbbf35b960229f32637676647bef873485df09" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6bf237d23440fb0b340f4336695f2a08c6b785aa98288b3313526e76c38bca19" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "613a7c50dbea57860eae686d580f83867582ffdadd63f0f3ebe6a85455ab7706" default)))
  '(debug-on-error nil)
  '(org-agenda-files
    (quote
     ("~/org/satsmtar.org" "~/docs/studium/proseminar/notes.org" "/home/moritz/org/bachelor.org" "/home/moritz/org/einkauf.org" "/home/moritz/org/ghcerrors.org" "/home/moritz/org/hie.org" "/home/moritz/org/job.org" "/home/moritz/org/notes.org" "/home/moritz/org/org.org" "/home/moritz/org/recipe.org" "/home/moritz/org/travel.org" "/home/moritz/org/typetheory.org")))
  '(package-selected-packages
    (quote
-    (magit-gh-pulls ess disaster geiser graphviz-dot-mode ws-butler bison-mode org-ref outline-magic outshine julia-shell julia-mode irfc keyfreq yaml-mode which-key web-mode volume use-package undo-tree unbound tuareg travis toml-mode sx sml-mode smart-mode-line rust-mode rainbow-delimiters prop-menu projectile pos-tip pkgbuild-mode pdf-tools paredit paradox nyan-mode nginx-mode multiple-cursors moe-theme mmm-mode magit-svn lua-mode llvm-mode libmpdee ledger-mode json-mode jonprl-mode irony-eldoc gnuplot github-issues flycheck-package flycheck-irony flx expand-region eww-lnum ensime emmet-mode elnode elisp-slime-nav dockerfile-mode docker dash-functional counsel company-irony company-coq company-cmake cmake-ide clang-format cdlatex cask auctex aggressive-indent ace-window)))
+    (smartparens racket-mode ivy-hydra swiper magit-gh-pulls ess disaster geiser graphviz-dot-mode ws-butler bison-mode org-ref outline-magic outshine julia-shell julia-mode irfc keyfreq yaml-mode which-key web-mode volume use-package undo-tree unbound tuareg travis toml-mode sx sml-mode smart-mode-line rust-mode rainbow-delimiters prop-menu projectile pos-tip pkgbuild-mode pdf-tools paredit paradox nyan-mode nginx-mode multiple-cursors moe-theme mmm-mode magit-svn lua-mode llvm-mode libmpdee ledger-mode json-mode jonprl-mode irony-eldoc gnuplot github-issues flycheck-package flycheck-irony flx expand-region eww-lnum ensime emmet-mode elnode elisp-slime-nav dockerfile-mode docker dash-functional counsel company-irony company-coq company-cmake cmake-ide clang-format cdlatex cask auctex aggressive-indent ace-window)))
  '(safe-local-variable-values
    (quote
-    ((projectile-project-compilation-cmd . "cd Horn/LLVM/reve/build && make -j8")
+    ((haskell-process-args-ghci "ghci" "--with-ghc" "ghci-ng")
+     (haskell-process-path-ghci . "stack")
+     (haskell-process-type . ghci)
+     (haskell-indent-spaces . 2)
+     (projectile-project-compilation-cmd . "cd Horn/LLVM/reve/build && make -j8")
      (buffer-file-coding-system . utf-8-unix)
      (eval remove-hook
            (quote write-contents-functions)
@@ -75,6 +79,4 @@
  ;; If there is more than one, they won't work right.
  )
 
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
 (load "~/.emacs.d/emacs.el")
