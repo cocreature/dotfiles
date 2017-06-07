9a60ce1091cba0966e9a5d17170311d036e44604
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-06-07 07:09:22 +0200

    Add webpaste

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(bold ((t (:weight ultra-bold)))))
 
 (load "~/.emacs.d/emacs.el")
 (custom-set-variables
@@ -17,7 +17,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
+    (webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
  '(safe-local-variable-values
    (quote
     ((eval if
