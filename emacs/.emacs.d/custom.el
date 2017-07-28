3e84dc8cbb34a1c8ad22f7ccea82135fe3e8016e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-07-28 07:34:39 +0200

    Update custom settings

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -9,9 +9,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(package-selected-packages
    (quote
-    (webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
+    (nix-mode webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
  '(safe-local-variable-values
    (quote
     ((buffer-file-coding-system . utf-8-unix)
