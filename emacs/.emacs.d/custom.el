18bdfd80b2d434e006fc292a242c65aaf62c8b55
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-11-07 19:47:52 +0100

    Update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -12,53 +12,54 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(package-selected-packages
-(quote
- (go-mode rjsx-mode js2-mode doom-themes dired-subtree nix-mode dante webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
-'(safe-local-variable-values
-(quote
- ((haskell-stylish-on-save . t)
-  (ispell-dictionary . "en_US")
-  (eval
-   (lambda nil
-     (when
-         (string=
-          (file-name-extension
-           (or
+   (quote
+    (go-mode rjsx-mode js2-mode doom-themes dired-subtree nix-mode dante webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
+ '(safe-local-variable-values
+   (quote
+    ((intero-targets "hsfmt:lib" "hsfmt:exe:hs-fmt" "hsfmt:test:properties")
+     (haskell-stylish-on-save . t)
+     (ispell-dictionary . "en_US")
+     (eval
+      (lambda nil
+        (when
+            (string=
+             (file-name-extension
+              (or
+               (buffer-file-name)
+               ""))
+             "v")
+          (verilog-mode))))
+     (eval
+      (lambda nil
+        (when
+            (or
+             (string=
+              (file-name-extension
+               (buffer-file-name))
+              "v")
+             nil)
+          (verilog-mode))))
+     (eval
+      (lambda nil
+        (when
+            (string=
+             (file-name-extension
+              (buffer-file-name))
+             "v")
+          (verilog-mode))))
+     (eval lambda nil
+           (message
+            (buffer-file-name)))
+     (eval setq auto-mode-alist
+           (\`
+            (("\\.v" . verilog-mode)
+             \, auto-mode-alist)))
+     (buffer-file-coding-system . utf-8-unix)
+     (eval if
+           (and
             (buffer-file-name)
-            ""))
-          "v")
-       (verilog-mode))))
-  (eval
-   (lambda nil
-     (when
-         (or
-          (string=
-           (file-name-extension
-            (buffer-file-name))
-           "v")
-          nil)
-       (verilog-mode))))
-  (eval
-   (lambda nil
-     (when
-         (string=
-          (file-name-extension
-           (buffer-file-name))
-          "v")
-       (verilog-mode))))
-  (eval lambda nil
-        (message
-         (buffer-file-name)))
-  (eval setq auto-mode-alist
-        (\`
-         (("\\.v" . verilog-mode)
-          \, auto-mode-alist)))
-  (buffer-file-coding-system . utf-8-unix)
-  (eval if
-        (and
-         (buffer-file-name)
-         (string-equal
-          (file-name-extension
-           (buffer-file-name))
-          "h"))
-        (c++-mode))))))
+            (string-equal
+             (file-name-extension
+              (buffer-file-name))
+             "h"))
+           (c++-mode))))))
