e8f8327075816fefa9a7a5be375cfa87e8f9c6c7
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-12-13 17:44:40 +0100

    Update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -16,7 +16,50 @@
     (go-mode rjsx-mode js2-mode doom-themes dired-subtree nix-mode dante webpaste magithub all-the-icons ggtags cmake-project cdlatex magit-svn yaml-mode ws-butler writeroom-mode which-key web-mode web volume use-package undo-tree unbound tuareg toml-mode systemd sml-mode smartparens rust-mode rainbow-delimiters racket-mode projectile powerline pkgbuild-mode pdf-tools org-bullets nginx-mode multiple-cursors mmm-mode markdown-mode magit lua-mode llvm-mode libmpdee ledger-mode keyfreq json-mode jonprl-mode ivy-hydra intero idris-mode hydra gnuplot git-gutter-fringe flycheck-package f expand-region ensime elisp-slime-nav dockerfile-mode dash-functional counsel company-coq cmake-mode clang-format bison-mode auctex ace-window)))
  '(safe-local-variable-values
    (quote
-    ((intero-targets "hsfmt:lib" "hsfmt:exe:hs-fmt" "hsfmt:test:properties")
+    ((intero-targets "irbuilder:lib" "irbuilder:test:irbuilder")
+     (intero-targets "servant-match:lib" "servant-match:test:test")
+     (eval let
+           ((default-directory
+              (locate-dominating-file buffer-file-name ".dir-locals.el")))
+           (setq-local coq-prog-args
+                       (\`
+                        ("-coqlib"
+                         (\,
+                          (expand-file-name ".."))
+                         "-R"
+                         (\,
+                          (expand-file-name "."))
+                         "Coq")))
+           (setq-local coq-prog-name
+                       (expand-file-name "../bin/coqtop")))
+     (eval progn
+           (let
+               ((coq-root-directory
+                 (when buffer-file-name
+                   (locate-dominating-file buffer-file-name ".dir-locals.el")))
+                (coq-project-find-file
+                 (and
+                  (boundp
+                   (quote coq-project-find-file))
+                  coq-project-find-file)))
+             (set
+              (make-local-variable
+               (quote tags-file-name))
+              (concat coq-root-directory "TAGS"))
+             (setq camldebug-command-name
+                   (concat coq-root-directory "dev/ocamldebug-coq"))
+             (unless coq-project-find-file
+               (set
+                (make-local-variable
+                 (quote compile-command))
+                (concat "make -C " coq-root-directory))
+               (set
+                (make-local-variable
+                 (quote compilation-search-path))
+                (cons coq-root-directory nil)))
+             (when coq-project-find-file
+               (setq default-directory coq-root-directory))))
+     (intero-targets "hsfmt:lib" "hsfmt:exe:hs-fmt" "hsfmt:test:properties")
      (haskell-stylish-on-save . t)
      (ispell-dictionary . "en_US")
      (eval
