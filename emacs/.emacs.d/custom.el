bd7a82d54b2e7b4657307f14806009e3bf384756
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-01-24 21:23:08 +0100

    Add more packages to package-selected-packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -9,4 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (font-lock+ all-the-icons use-package))))
+ '(package-selected-packages
+   (quote
+    (all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package))))
