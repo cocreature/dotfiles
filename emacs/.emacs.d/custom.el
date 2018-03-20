dc8e6e744e540cd1b2596b5cd5493e7522236dbf
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-03-20 14:49:31 +0100

    Disable lsp

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,6 +14,7 @@
     (csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package)))
  '(safe-local-variable-values
    (quote
-    ((ispell-dictionary . "en_GB")
+    ((buffer-file-coding-system . utf-8-unix)
+     (ispell-dictionary . "en_GB")
      (TeX-source-correlate-method-active . synctex)
      (TeX-command-extra-options . "-shell-escape")))))
