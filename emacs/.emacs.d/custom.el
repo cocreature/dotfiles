0a02f8db599531f71b9a89a4e517128c5e89af0e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-03-28 08:56:38 +0200

    Add haskell-stylish-on-save to safe-local-variable-values

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,7 +14,8 @@
     (academic-phrases wgrep yasnippet-snippets gist langtool flyspell-correct-ivy csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package)))
  '(safe-local-variable-values
    (quote
-    ((buffer-file-coding-system . utf-8-unix)
+    ((haskell-stylish-on-save . t)
+     (buffer-file-coding-system . utf-8-unix)
      (ispell-dictionary . "en_GB")
      (TeX-source-correlate-method-active . synctex)
      (TeX-command-extra-options . "-shell-escape")))))
