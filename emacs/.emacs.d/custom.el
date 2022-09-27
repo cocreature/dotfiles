5239627f176e146c674a14111d5866eebcc53324
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-09-27 09:18:27 +0200

    drop git gutter

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -11,10 +11,38 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (intero glsl-mode ivy-xref academic-phrases wgrep yasnippet-snippets gist langtool flyspell-correct-ivy csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package)))
+    (bazel jenkins typescript-mode bazel-mode intero glsl-mode ivy-xref academic-phrases wgrep yasnippet-snippets gist langtool flyspell-correct-ivy csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package)))
  '(safe-local-variable-values
    (quote
-    ((intero-targets "servant-match:lib" "servant-match:test:test")
+    ((eval set-input-method "TeX")
+     (haskell-indent-spaces . 4)
+     (haskell-process-type . cabal-repl)
+     (eval c-set-offset
+           (quote access-label)
+           (quote -))
+     (eval c-set-offset
+           (quote substatement-open)
+           0)
+     (eval c-set-offset
+           (quote arglist-cont-nonempty)
+           (quote +))
+     (eval c-set-offset
+           (quote arglist-cont)
+           0)
+     (eval c-set-offset
+           (quote arglist-intro)
+           (quote +))
+     (eval c-set-offset
+           (quote inline-open)
+           0)
+     (eval c-set-offset
+           (quote defun-open)
+           0)
+     (eval c-set-offset
+           (quote innamespace)
+           0)
+     (indicate-empty-lines . t)
+     (intero-targets "servant-match:lib" "servant-match:test:test")
      (haskell-stylish-on-save . t)
      (buffer-file-coding-system . utf-8-unix)
      (ispell-dictionary . "en_GB")
