ebc0c20c6b34f2a150054a068a8d522bd05d2233
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2023-02-02 15:48:22 +0100

    add editorconfig

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -10,41 +10,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (bazel jenkins typescript-mode bazel-mode intero glsl-mode ivy-xref academic-phrases wgrep yasnippet-snippets gist langtool flyspell-correct-ivy csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package)))
+   '(editorconfig bazel jenkins typescript-mode bazel-mode intero glsl-mode ivy-xref academic-phrases wgrep yasnippet-snippets gist langtool flyspell-correct-ivy csharp-mode magit-svn all-the-icons counsel-projectile dired-subtree company ws-butler undo-tree ivy-hydra counsel swiper ivy use-package))
  '(safe-local-variable-values
-   (quote
-    ((eval set-input-method "TeX")
+   '((eval set-input-method "TeX")
      (haskell-indent-spaces . 4)
      (haskell-process-type . cabal-repl)
-     (eval c-set-offset
-           (quote access-label)
-           (quote -))
-     (eval c-set-offset
-           (quote substatement-open)
-           0)
-     (eval c-set-offset
-           (quote arglist-cont-nonempty)
-           (quote +))
-     (eval c-set-offset
-           (quote arglist-cont)
-           0)
-     (eval c-set-offset
-           (quote arglist-intro)
-           (quote +))
-     (eval c-set-offset
-           (quote inline-open)
-           0)
-     (eval c-set-offset
-           (quote defun-open)
-           0)
-     (eval c-set-offset
-           (quote innamespace)
-           0)
+     (eval c-set-offset 'access-label '-)
+     (eval c-set-offset 'substatement-open 0)
+     (eval c-set-offset 'arglist-cont-nonempty '+)
+     (eval c-set-offset 'arglist-cont 0)
+     (eval c-set-offset 'arglist-intro '+)
+     (eval c-set-offset 'inline-open 0)
+     (eval c-set-offset 'defun-open 0)
+     (eval c-set-offset 'innamespace 0)
      (indicate-empty-lines . t)
      (intero-targets "servant-match:lib" "servant-match:test:test")
      (haskell-stylish-on-save . t)
      (buffer-file-coding-system . utf-8-unix)
      (ispell-dictionary . "en_GB")
      (TeX-source-correlate-method-active . synctex)
-     (TeX-command-extra-options . "-shell-escape")))))
+     (TeX-command-extra-options . "-shell-escape"))))
